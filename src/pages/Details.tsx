a84b0666aaae4ef74b9bea36a3ce8ce144cbcb82
paimdev <lucas@paim.dev>
2025-01-13 11:28:49 +0000

    add Details component to display movie information and video player

---
+++ src/pages/Details.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import VideoPlayer from "../components/VideoPlayer";
+import { fetchMovieDetails } from "../services/api";
+import { Movie } from "../types/Movie";
+
+const Details: React.FC = () => {
+    const { id } = useParams<{ id: string }>();
+    const [movie, setMovie] = useState<Movie | null>(null);
+
+    useEffect(() => {
+        const loadDetails = async () => {
+            if (id) setMovie(await fetchMovieDetails(id));
+        };
+        loadDetails();
+    }, [id]);
+
+    if (!movie) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
+            <button>Watch Movie</button>
+            <VideoPlayer videoUrl="https://rawgit.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4" />
+        </div>
+    );
+};
+
+export default Details;
