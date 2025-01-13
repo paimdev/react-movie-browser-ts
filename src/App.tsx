cd767e1ce32b0005c2bddfcf616f0fd7fcf69bb8
paimdev <lucas@paim.dev>
2025-01-13 13:05:30 +0000

    style: import global CSS styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import "./styles/global.css";
 
 const App: React.FC = () => (
   <BrowserRouter>
