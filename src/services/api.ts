13301438a133e22ce3a2d68886c56571af5994ba
paimdev <lucas@paim.dev>
2025-01-13 11:24:38 +0000

    add API service functions to fetch movies and genres

---
+++ src/services/api.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Movie, Genre } from "../types/Movie.ts";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const fetchMovies = async (type: string): Promise<Movie[]> => {
+  const response = await axios.get(`${BASE_URL}/${type}/popular`, {
+    params: { api_key: API_KEY },
+  });
+  return response.data.results;
+};
+
+export const fetchGenres = async (): Promise<Genre[]> => {
+  const response = await axios.get(`${BASE_URL}/genre/movie/list`, {
+    params: { api_key: API_KEY },
+  });
+  return response.data.genres;
+};
+
+export const fetchMovieDetails = async (id: string): Promise<Movie> => {
+  const response = await axios.get(`${BASE_URL}/movie/${id}`, {
+    params: { api_key: API_KEY },
+  });
+  return response.data;
+};
